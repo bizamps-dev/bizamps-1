8e6a266ed4e382850262a3e23c5c2d53d6b10722
shades-7 <salonimark7@gmail.com>
2020-09-24 16:13:52 +0530

    template blank

--- src/pages/templet.js
+++ src/pages/templet.js
@@ -1,117 +1,132 @@
-import React from 'react';
-import { Link,graphql } from "gatsby"
-import styles from "../stylesheets/templet.module.scss"
-import BackgroundImage from 'gatsby-background-image'
-import Img from 'gatsby-image'
+// import React from 'react';
+// import { Link,graphql } from "gatsby"
+// import styles from "../stylesheets/templet.module.scss"
+// import BackgroundImage from 'gatsby-background-image'
+// import Img from 'gatsby-image'
 
-import Layout from '../components/layout'
-import Navbar from '../components/navbar/Navbar'
-import BlogContent from '../components/BlogTemplet/BlogContent'
-import BlogPerson from '../components/BlogTemplet/BlogPerson'
-import Form from '../components/BlogTemplet/BlogForm'
-import Location from '../components/Location/Location'
-import Footer from '../components/Footer/footer'
+// import Layout from '../components/layout'
+// import Navbar from '../components/navbar/Navbar'
+// import BlogContent from '../components/BlogTemplet/BlogContent'
+// import BlogPerson from '../components/BlogTemplet/BlogPerson'
+// import Form from '../components/BlogTemplet/BlogForm'
+// import Location from '../components/Location/Location'
+// import Footer from '../components/Footer/footer'
+
+
+// const Templet = (props) => {
+//     return(
+//         <>
+//             <Layout>
+//                 <BackgroundImage fluid={props.data.bg1.childImageSharp.fluid} className={styles.container1}>
+//                     <Navbar logo={props.data.bizampsLogo.childImageSharp.fluid} />
+//                 </BackgroundImage >
+
+//                 {/* <BackgroundImage fluid={props.data.bg2.childImageSharp.fluid} className={styles.container2}>
+//                     <Navbar logo={props.data.bizampsLogo.childImageSharp.fluid} />
+//                     <Img fluid={props.data.drone.childImageSharp.fluid} className={styles.drone}/>
+//                 </BackgroundImage > */}
+
+         
+//             <div className={styles.blogContainer}>
+//                  <BlogContent 
+//                  illus1={props.data.pic1.childImageSharp.fluid}
+//                  illus2={props.data.pic2.childImageSharp.fluid}/>
+//              </div> 
+
+//             <div className={styles.personContainer}>
+//                 <BlogPerson person={props.data.person.childImageSharp.fluid}/>
+//             </div>
+
+//             <div className={styles.formcontainer}>
+//                 <Form/>
+//             </div>
+
+//             <div className={styles.location1}>
+//               <Location  first="Home" second="Expertise" third="[Blog] Value In Sales? "/>
+//             </div>
+
+//             <Footer />
+
+//             </Layout>
+//         </>
+//     )
+// }
+
+// export default Templet;
+
+// export const BlogPageQuery = graphql`
+//   query {
+    
+//     bizampsLogo: file(relativePath: { eq: "bizampsLogo.png" }) {
+//         childImageSharp {
+//           fluid(maxWidth: 1000) {
+//             ...GatsbyImageSharpFluid
+//           }
+//         }
+//       }
+
+//     bg1: file(relativePath: { eq: "Blogbg.png" }) {
+//       childImageSharp {
+//         fluid(maxWidth: 1000) {
+//           ...GatsbyImageSharpFluid
+//         }
+//       }
+//     }
+
+//     bg2: file(relativePath: { eq: "MobBlogbg.png" }) {
+//       childImageSharp {
+//         fluid(maxWidth: 1000) {
+//           ...GatsbyImageSharpFluid
+//         }
+//       }
+//     }
+
+//     drone: file(relativePath: { eq: "Drone.png" }) {
+//       childImageSharp {
+//         fluid(maxWidth: 1000) {
+//           ...GatsbyImageSharpFluid
+//         }
+//       }
+//     }
+
+//     pic1: file(relativePath: { eq: "BlogIllustration1.png" }) {
+//       childImageSharp {
+//         fluid(maxWidth: 1000) {
+//           ...GatsbyImageSharpFluid
+//         }
+//       }
+//     }
+
+//     pic2: file(relativePath: { eq: "BlogIllustration2.png" }) {
+//       childImageSharp {
+//         fluid(maxWidth: 1000) {
+//           ...GatsbyImageSharpFluid
+//         }
+//       }
+//     }
+
+//     person: file(relativePath: { eq: "person.png" }) {
+//       childImageSharp {
+//         fluid(maxWidth: 1000) {
+//           ...GatsbyImageSharpFluid
+//         }
+//       }
+//     }
+
+//   }
+// `;
+
+
+import React from 'react';
 
 
 const Templet = (props) => {
     return(
         <>
-            <Layout>
-                <BackgroundImage fluid={props.data.bg1.childImageSharp.fluid} className={styles.container1}>
-                    <Navbar logo={props.data.bizampsLogo.childImageSharp.fluid} />
-                </BackgroundImage >
-
-                {/* <BackgroundImage fluid={props.data.bg2.childImageSharp.fluid} className={styles.container2}>
-                    <Navbar logo={props.data.bizampsLogo.childImageSharp.fluid} />
-                    <Img fluid={props.data.drone.childImageSharp.fluid} className={styles.drone}/>
-                </BackgroundImage > */}
-
-         
-            <div className={styles.blogContainer}>
-                 <BlogContent 
-                 illus1={props.data.pic1.childImageSharp.fluid}
-                 illus2={props.data.pic2.childImageSharp.fluid}/>
-             </div> 
-
-            <div className={styles.personContainer}>
-                <BlogPerson person={props.data.person.childImageSharp.fluid}/>
-            </div>
-
-            <div className={styles.formcontainer}>
-                <Form/>
-            </div>
-
-            <div className={styles.location1}>
-              <Location  first="Home" second="Expertise" third="[Blog] Value In Sales? "/>
-            </div>
-
-            <Footer />
-
-            </Layout>
+           <p>hi i am working</p>
         </>
     )
 }
 
 export default Templet;
 
-export const BlogPageQuery = graphql`
-  query {
-    
-    bizampsLogo: file(relativePath: { eq: "bizampsLogo.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 1000) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-
-    bg1: file(relativePath: { eq: "Blogbg.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    bg2: file(relativePath: { eq: "MobBlogbg.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    drone: file(relativePath: { eq: "Drone.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    pic1: file(relativePath: { eq: "BlogIllustration1.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    pic2: file(relativePath: { eq: "BlogIllustration2.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    person: file(relativePath: { eq: "person.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-  }
-`;
